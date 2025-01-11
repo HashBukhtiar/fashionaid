a8582038ab84e7fabb0eff436226d34a5b7c21db
Andrew De Rango <drewderango@gmail.com>
2025-01-11 14:39:40 -0500

    add image

--- web/src/pages/LandingPage.js
+++ web/src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/LandingPage.css';
+import logo from '../images/logo.png';
 
 function LandingPage() {
   return (
     <div className="LandingPage">
       <header className="LandingPage-header">
+        <img src={logo} className="LandingPage-logo" alt="logo" />
         <h1>Welcome to Looksmaxxer</h1>
         <p>Your journey to looking your best starts here.</p>
         <div className="LandingPage-buttons">
