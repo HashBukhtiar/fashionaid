3b49e0791f65ba9cf4ca05dc23f6fe20f2690536
HashBukhtiar <hashbukhtiar@gmail.com>
2025-01-11 18:16:38 -0500

    fixed issue where styles weren't imported correctly

--- web/src/pages/AIChatPage.js
+++ web/src/pages/AIChatPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/AIChatPage.css';
+import './AIChatPage.css';
 
 const AIChatPage = () => {
   const [messages, setMessages] = useState([]);
