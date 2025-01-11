fce1e392d3270a06e211504501443558bdad0a41
Andrew De Rango <drewderango@gmail.com>
2025-01-11 17:09:53 -0500

    add back button

--- web/src/pages/LoginPage.js
+++ web/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 import '../styles/LoginPage.css';
 import logo from '../images/logo.png';
 
@@ -6,6 +7,9 @@ function LoginPage() {
   return (
     <div className="LoginPage">
       <header className="LoginPage-header">
+        <button className="LoginPage-backButton">
+          <FaArrowLeft />
+        </button>
         <img src={logo} className="LoginPage-logo" alt="logo" />
         <h1>Login</h1>
         <input type="text" className="LoginPage-input" placeholder="Username" />
