120b2c5450154e844900fba836dc0d0b982971b5
HashBukhtiar <hashbukhtiar@gmail.com>
2025-01-11 17:19:18 -0500

    fixed issue with vertex ai not receving image from local computer

--- web/src/ai/vertex.js
+++ web/src/ai/vertex.js
@@ -1,16 +1,17 @@
 const { VertexAI } = require('@google-cloud/vertexai');
-const readline = require('readline');
+const path = require('path');
+const { convertImageToInlineData } = require('./image');
 
 // Initialize Vertex with your Cloud project and location
-const vertex_ai = new VertexAI({ project: 'looksmaxxer', location: 'northamerica-northeast1' });
+const vertex_ai = new VertexAI({ project: 'hlthy-421200', location: 'northamerica-northeast1' });
 const model = 'gemini-1.5-flash-001';
 
 const textsi_1 = {
   text: `State everything possible about what the person is wearing in this JSON format. Try to find the most dominant hex code.
-  {article1: color1, article2: color2} 
-  
-  For example:
-  {shirt: #333333, pants: #d9b68a}`
+{article1: color1, article2: color2}
+
+For example:
+{shirt: #333333, pants: #d9b68a}`
 };
 
 // Instantiate the models
@@ -44,28 +45,21 @@ const generativeModel = vertex_ai.preview.getGenerativeModel({
   },
 });
 
+// Convert image to inline data
+const image1_1 = convertImageToInlineData('', 'image/jpeg');
+
 const chat = generativeModel.startChat({});
 
-async function sendMessage(message) {
-  try {
-    const streamResult = await chat.sendMessageStream(message);
-    const response = (await streamResult.response).candidates[0].content;
-    process.stdout.write('Stream result: ' + JSON.stringify(response) + '\n');
-  } catch (error) {
-    console.error('Error sending message:', error);
-  }
+async function sendMessage(messages) {
+  const streamResult = await chat.sendMessageStream(messages);
+  process.stdout.write('stream result: ' + JSON.stringify((await streamResult.response).candidates[0].content) + '\n');
 }
 
 async function generateContent() {
-  const samplePrompts = [
-    "https://drive.google.com/file/d/15_FHnuF9wlMBDBdTsCUS0ZB5T7SCEoAw/view?usp=sharing",
-    "https://drive.google.com/file/d/1_AuLFWE9N8eYDHHrNNsjK5E7fiA_6G8x/view?usp=sharing"
-  ];
-
-  for (const prompt of samplePrompts) {
-    console.log(`Sending prompt: "${prompt}"`);
-    await sendMessage(prompt);
-  }
+  await sendMessage([
+    textsi_1,
+    image1_1
+  ]);
 }
 
 generateContent();
