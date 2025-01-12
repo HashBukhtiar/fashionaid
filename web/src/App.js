b40e9ce15de89ccc1ff89eb9b88be6e739c7662f
Andrew De Rango <drewderango@gmail.com>
2025-01-11 19:16:34 -0500

    add signup page

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import './App.css';
 import AIChatPage from './pages/AIChatPage';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" element={<LandingPage />} />
           <Route path="/chat" element={<AIChatPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </div>
     </Router>
